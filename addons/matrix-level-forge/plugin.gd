091d21fc105f018b5c3b21dfeee726609e2c279f
Null <LincolnHutchinson@outlook.com>
2018-10-08 21:31:04 -0700

    Made code less focused on the existence of Alloi. Began converting files into something that would be more generally useful for the average dev.

--- addons/matrix-level-forge/plugin.gd
+++ addons/matrix-level-forge/plugin.gd
@@ -2,9 +2,26 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	ProjectSettings.set_setting("Matrix Level Forge/Test", 1)
+	ProjectSettings.set_setting("matrix_level_forge/editor_scene", "res://addons/matrix-level-forge/LevelEditor.tscn")
+	var scene_settings = {
+		"name": "matrix_level_forge/editor_scene",
+		"type": TYPE_STRING,
+		"hint": PROPERTY_HINT_FILE,
+		"hint_string": "*.tscn"
+	}
+	ProjectSettings.add_property_info(scene_settings)
+	
+	ProjectSettings.set_setting("matrix_level_forge/level_template", "res://addons/matrix-level-forge/LevelTemplate.tscn")
+	var template_settings = {
+		"name": "matrix_level_forge/level_template",
+		"type": TYPE_STRING,
+		"hint": PROPERTY_HINT_FILE,
+		"hint_string": "*.tscn"
+	}
+	ProjectSettings.add_property_info(template_settings)
 	ProjectSettings.save()
 
 func _exit_tree():
-	ProjectSettings.set_setting("Matrix Level Forge/Test", null)
+	ProjectSettings.set_setting("matrix_level_forge/editor_scene", null)
+	ProjectSettings.set_setting("matrix_level_forge/level_template", null)
 	ProjectSettings.save()
